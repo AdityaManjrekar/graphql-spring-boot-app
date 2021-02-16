2106fc0d59cf961d1d3ac47555aebb53c03bc346
adityamanjrekar <aditya.manjrekar27@gmail.com>
2021-02-15 20:22:02 -0800

    Added logging listener

---
+++ src/main/java/com/graphql/app/listener/LoggingListener.java
@@ -0,0 +1,45 @@
+package com.graphql.app.listener;
+
+import graphql.kickstart.servlet.core.GraphQLServletListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.var;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LoggingListener implements GraphQLServletListener {
+
+    private final Clock clock;
+
+    @Override
+    public RequestCallback onRequest(HttpServletRequest request, HttpServletResponse response) {
+
+        var startTime = Instant.now(clock);
+
+        return new RequestCallback() {
+            @Override
+            public void onSuccess(HttpServletRequest request, HttpServletResponse response) {
+
+            }
+
+            @Override
+            public void onError(HttpServletRequest request, HttpServletResponse response, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onFinally(HttpServletRequest request, HttpServletResponse response) {
+
+                log.info("Completed Request, Time take {}", Duration.between(startTime,Instant.now(clock)));
+            }
+        };
+    }
+}
